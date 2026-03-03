afa025eb676cd335effef9ad367a82c5d83f5a35
Degamo John R. <143172157+chounoydev@users.noreply.github.com>
2026-03-02 23:40:42 -0800

    Change logo image source in Footer
    Updated logo image source in Footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <div>
           <div className="flex items-center gap-4 mb-4">
             <img
-              src="/albertII.png"
+              src="smonk.jpg"
               alt="SpaceMonkey Logo"
               className="w-12 h-12 rounded-full object-cover border border-gray-300"
             />
@@ -103,3 +103,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
